d91292d9a02240a40385926fbeeae08062272855
JT4848 <jtpricone@gmail.com>
2023-07-14 02:00:28 -0400

    done but need to clean up

--- promt.md
+++ promt.md
@@ -1,6 +1,6 @@
 Workshop Prompt
-DONE-Retrieve all pokemon (first 20)
-DONE-Display all pokemon
+Retrieve all pokemon (first 20)
+Display all pokemon
 When you click on a pokemon in the list, retrieve the single pokemon
 display single pokemon details
 Push up code to GitHub
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+//get all pokemon
 const allPokemon = async () => {
   try{
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
@@ -10,28 +13,60 @@ const allPokemon = async () => {
   }
 }
 
-
+//render pokemon and getting single pokemon
 const renderPokemon = (pokeArr) => {
   const pokeNames = pokeArr.map((pokemon) => {
     const poke = pokemon.url.split(`/`);
     const pokeNum = poke[6];
     return `<a id="myA" data-poke-num="${pokeNum}" href=""><li>${pokemon.name}</li><a>`;
   }).join(``);
-  // console.log(pokeNames);
+  // console.log(pokeArr);
   document.querySelector(`ul`).innerHTML = pokeNames;
-
-  const links = document.querySelectorAll(`#myA`);
-  // console.log(links)
-  for(let i = 0; i < links.length; i++){
-    links[i].addEventListener(`click`, async (event) => {
-      event.preventDefault();
-      const number = links[i].dataset.pokeNum;
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
-      const data = await response.json();
-      console.log(`hello`);
-    })
-  }
 }
 
-allPokemon();
 
+const clickingPoke = () => {
+  const links = document.querySelectorAll(`#myA`);
+    // console.log(links)
+    for(let i = 0; i < links.length; i++){
+      links[i].addEventListener(`click`, async (event) => {
+        event.preventDefault();
+        const number = links[i].dataset.pokeNum;
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+        const data = await response.json();
+        console.log(data);
+        const ulReset = document.querySelector('ul');
+        ulReset.innerHTML = '';
+        let detailsHtml = `<h1>${data.name}</h1>`
+        document.querySelector(`#details`).innerHTML = detailsHtml;
+        let imgHtml = `<img src = "${data.sprites.front_shiny}">`;
+        document.querySelector(`#pokeImg`).innerHTML = imgHtml;
+        let pokeButton = `<button>Go back</button>`;
+        document.querySelector(`#backButton`).innerHTML = pokeButton;
+        
+        document.querySelector(`#backButton`).addEventListener(`click`, async (event) => {
+          event.preventDefault();
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+          const data = await response.json();
+          // console.log(data.results);
+          renderPokemon(data.results);
+          const nameReset = document.querySelector(`#details`);
+          nameReset.innerHTML = ``;
+          const imgReset = document.querySelector(`#pokeImg`);
+          imgReset.innerHTML = ``;
+          const buttonReset = document.querySelector(`#backButton`);
+          buttonReset.innerHTML = ``;
+        })
+        
+      })
+    }
+}
+
+
+const init = async () => {
+  await allPokemon()
+ 
+  clickingPoke();
+
+}
+init();
